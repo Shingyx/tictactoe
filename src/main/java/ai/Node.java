e6ea9ef241a279778a2afa93f4b2159833301b39
Su-Shing Chen <su-shing_chen@hotmail.co.nz>
2016-02-08 22:47:22 +1300

    Node for the MiniMax algorithm.

---
+++ src/main/java/ai/Node.java
@@ -0,0 +1,62 @@
+package ai;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Node for the MiniMax algorithm.
+ */
+public class Node {
+    private int value = -1;
+    private ArrayList<Node> children;
+
+    public Node(int value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    public Node(Node... nodes) {
+        this.value = -1;
+        this.children = new ArrayList<>();
+        Collections.addAll(children, nodes);
+    }
+
+    private int maxValue(int alpha, int beta) {
+        if (children.size() == 0) {
+            return this.value;
+        }
+        int result = Integer.MIN_VALUE;
+        for (Node node : children) {
+            result = Math.max(result, node.minValue(alpha, beta));
+            if (result >= beta) {
+                break;
+            }
+            alpha = Math.max(alpha, result);
+        }
+        return result;
+    }
+
+    private int minValue(int alpha, int beta) {
+        if (children.size() == 0) {
+            return this.value;
+        }
+        int result = Integer.MAX_VALUE;
+        for (Node node : children) {
+            result = Math.min(result, node.maxValue(alpha, beta));
+            if (result <= alpha) {
+                break;
+            }
+            beta = Math.min(beta, result);
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        if (children.size() == 0) {
+            return String.valueOf(this.value);
+        } else {
+            return "[" + children.toString() + "]";
+        }
+    }
+}
